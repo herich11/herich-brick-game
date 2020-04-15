17bcddb58102a6e396b1a0efd63bd3702c25cb70
unknown <hericahyononew@gmail.com>
2020-04-15 23:31:14 +0700

    first commit

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1586920434,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/block-sheet0.png",
+		"images/table-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/bgscore-sheet0.png",
+		"images/title-sheet0.png",
+		"images/buttonopen-sheet0.png",
+		"images/buttonsend-sheet0.png",
+		"images/backmenu-sheet0.png",
+		"images/buttonstart-sheet0.png",
+		"images/buttonpauseresume-sheet0.png",
+		"media/sounddestroy.ogg",
+		"media/backsoundgame.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
